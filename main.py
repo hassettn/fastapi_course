23c5a7e3283f8a794a0a9b26844c17ee47c944ba
unknown <hassett.n1@gmail.com>
2024-02-28 14:29:14 -0500

    Add /request POST method

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 # main.py
 # course here: https://realpython.com/fastapi-python-web-apis
 
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class my_Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+# defines a structure like this:
+"""
+{
+    "name": "Foo",
+    "description": "An optional description",
+    "price": 45.2,
+    "tax": 3.5
+}
+"""
+
 
 my_app = FastAPI()
 
@@ -21,3 +41,7 @@ async def read_item(item_id):
 async def read_item(number: int):
     # data parsing and validation as integer
     return {"item_id": number}
+
+@my_app.post("/request/")
+async def create_item(request: my_Item):
+    return request
